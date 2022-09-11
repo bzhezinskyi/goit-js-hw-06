1f43c2bfb87c567493a557c3c04d63c92134614f
bzhezinskyi <108461894+bzhezinskyi@users.noreply.github.com>
2022-09-11 19:34:09 +0000

    Deploying to gh-pages from @ bzhezinskyi/goit-js-hw-06@ab779ae1d27bf2290da4df48b3e1fb90ae1a4a12 🚀

---
+++ task-01.7d43b389.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("#categories");console.log(`Number of categories: ${e.children.length}`);for(const l of e.children)console.log(`Category: ${l.firstElementChild.textContent}`),console.log(`Elements: ${l.lastElementChild.querySelectorAll("li").length}`);
+//# sourceMappingURL=task-01.7d43b389.js.map
